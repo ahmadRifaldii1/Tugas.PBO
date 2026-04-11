36790fbcbee0c2f0f0a96432aeefa748a5a80c56
ahmadRifaldii1 <luxxyone1@gmail.com>
2026-04-11 11:52:00 +0800

    Add files via upload

---
+++ Tugas1_PBO/Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,16 @@
+ /* NIM   : 13020240259
+Nama  : Ahmad Rifaldi
+Hari/Tanggal : 5 April 2026
+Waktu : 04:00*/
+public class Asgdll {
+public static void main(String[] args) {
+
+float f= 20.0f;
+double fll;
+
+fll=10.0f;
+
+System.out.println ("f : "+f + "\nf11: "+fll);
+
+}
+}
---
+++ Tugas1_PBO/Tugas1_SourceCode_Praktek/Asign.java
@@ -0,0 +1,15 @@
+ /* NIM   : 13020240259
+Nama  : Ahmad Rifaldi
+Hari/Tanggal : 5 April 2026
+Waktu : 04:00*/
+public class Asign {
+public static void main(String[] args) {
+
+int i;
+
+System.out.print ("hello\n"); i = 5;
+
+System.out.println ("Ini nilai i : " + i);
+
+}
+}
